9b896787f678eee7f231eb7330923c254c0b26f9
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-02-27 13:00:08 -0600

    working on styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import './Home.css';
 
 export class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Home :)</h1>
-            </div>
+            <>
+                <body id="homePage">
+                    <h1>Home :)</h1>
+                </body>
+            </>
         )
     }
 }
